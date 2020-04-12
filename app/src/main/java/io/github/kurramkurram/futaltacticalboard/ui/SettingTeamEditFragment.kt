8032d998ca4ebfd4ee75fcab9ae02f5ed904127b
Masataka.Goto <54581188+kurramkurram@users.noreply.github.com>
2020-04-12 18:48:26 +0900

    add spinner

--- app/src/main/java/io/github/kurramkurram/futaltacticalboard/ui/SettingTeamEditFragment.kt
+++ app/src/main/java/io/github/kurramkurram/futaltacticalboard/ui/SettingTeamEditFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.ImageView
 import android.widget.LinearLayout
+import android.widget.Spinner
 import androidx.fragment.app.Fragment
 import io.github.kurramkurram.futaltacticalboard.R
 
@@ -14,6 +16,7 @@ class SettingTeamEditFragment : Fragment(), View.OnClickListener {
     private lateinit var mPlayerEditLayout: LinearLayout
     private lateinit var mTeamRed: ImageView
     private lateinit var mTeamBlue: ImageView
+    private lateinit var mSpinner: Spinner
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,9 +29,19 @@ class SettingTeamEditFragment : Fragment(), View.OnClickListener {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         mTeamRed = view.findViewById(R.id.edit_player_red)
         mTeamRed.setOnClickListener(this)
+        mTeamRed.isEnabled = true
         mTeamBlue = view.findViewById(R.id.edit_player_blue)
         mTeamBlue.setOnClickListener(this)
+        mTeamBlue.isEnabled = true
         mPlayerEditLayout = view.findViewById(R.id.player_edit_layout)
+
+        val adapter = context?.let { ArrayAdapter<Int>(it, android.R.layout.simple_list_item_1) }
+        adapter!!.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        for (i in FutsalCortActivity.PLAYER_RED_ARRAY.indices) {
+            adapter.add(i + 1)
+        }
+        mSpinner = view.findViewById(R.id.player_number_spinner)
+        mSpinner.adapter = adapter
     }
 
     override fun onClick(v: View?) {
