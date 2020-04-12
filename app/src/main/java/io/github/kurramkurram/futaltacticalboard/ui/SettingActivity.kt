0a1de7c8fbfcbc53496ef03c655ce5b6eca659ac
Masataka.Goto <54581188+kurramkurram@users.noreply.github.com>
2020-04-12 11:43:52 +0900

    setting screen is changed by TabLayout

--- app/src/main/java/io/github/kurramkurram/futaltacticalboard/ui/SettingActivity.kt
+++ app/src/main/java/io/github/kurramkurram/futaltacticalboard/ui/SettingActivity.kt
@@ -1,21 +1,31 @@
 package io.github.kurramkurram.futaltacticalboard.ui
 
 import android.os.Bundle
+
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.NotificationCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentStatePagerAdapter
+import androidx.fragment.app.FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT
 import androidx.viewpager.widget.ViewPager
+
 import io.github.kurramkurram.futaltacticalboard.R
 
 class SettingActivity : AppCompatActivity() {
+
+    private val mFragmentManager = supportFragmentManager
+
+    companion object {
+        const val SETTING_CORT_EDIT = 0
+        const val SETTING_TEAM_EDIT = 1
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setting)
 
-        val pagerAdapter = SettingPagerAdapter()
+        val pagerAdapter =
+            SettingPagerAdapter(mFragmentManager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT)
         val pager = findViewById<ViewPager>(R.id.setting_view_pager)
         pager.adapter = pagerAdapter
     }
@@ -25,16 +35,20 @@ class SettingActivity : AppCompatActivity() {
 
         override fun getItem(position: Int): Fragment {
             return when (position) {
-                0 -> SettingCortEditFragment()
-                1 -> SettingTeamEditFragment()
-                else -> 
+                SETTING_CORT_EDIT -> SettingCortEditFragment()
+                SETTING_TEAM_EDIT -> SettingTeamEditFragment()
+                else -> SettingOthersFragment()
             }
         }
 
         override fun getCount(): Int = 3
 
         override fun getPageTitle(position: Int): CharSequence? {
-            return "pageTitle $position"
+            return when (position) {
+                SETTING_CORT_EDIT -> "CORT"
+                SETTING_TEAM_EDIT -> "TEAM"
+                else -> "OTHERS"
+            }
         }
     }
 }
--- app/src/main/java/io/github/kurramkurram/futaltacticalboard/ui/SettingCortEditFragment.kt
+++ app/src/main/java/io/github/kurramkurram/futaltacticalboard/ui/SettingCortEditFragment.kt
@@ -1,7 +1,22 @@
 package io.github.kurramkurram.futaltacticalboard.ui
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import io.github.kurramkurram.futaltacticalboard.R
 
 class SettingCortEditFragment : Fragment() {
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_cort_edit, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    }
 }
